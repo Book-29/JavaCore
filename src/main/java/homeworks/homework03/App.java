84d47a8c93cf03afcc152e88ab24f05ddf0aca4f
Лариса <book-29@mail.ru>
2025-01-08 15:48:25 +0300

    Домашнее задание 3, создание класса App

---
+++ src/main/java/homeworks/homework03/App.java
@@ -0,0 +1,37 @@
+package homeworks.homework03;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+            Television myTelevision = new Television();
+            myTelevision.setModel("Sony");
+            System.out.println(myTelevision.toString());
+
+            Television myTelevision2 = new Television();
+            myTelevision2.setModel("Sharp");
+            System.out.println(myTelevision2.toString());
+
+            Television myTelevision3 = new Television();
+             myTelevision3.setModel("Xiaomi");
+             System.out.println(myTelevision3.toString());
+
+             Television myTelevision4 = new Television();
+             myTelevision4.setModel("Samsung");
+             System.out.println(myTelevision4.toString());
+
+             Television myTelevision5 = new Television();
+             myTelevision5.setModel("Hyundai");
+             System.out.println(myTelevision5.toString());
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите модель TV: ");
+        String tv = scan.nextLine();
+        System.out.print(tv);
+
+
+
+
+    }
+}
