8329f158e78ade9854f7f904586895bf50739164
Лариса <book-29@mail.ru>
2025-01-08 21:21:39 +0300

    выполнение домашнего задания 6

--- src/main/java/homeworks/homework06/App.java
+++ src/main/java/homeworks/homework06/App.java
@@ -6,33 +6,68 @@ import java.util.regex.Pattern;
 
 public class App {
     public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-       String inputText = sc.nextLine();
 
-        Pattern pattern = Pattern.compile("([а-яА-Я]+)(?: ([а-яА-Я]+))? = (\\d+);");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите имя и количество денег: ");
+        String inputText = sc.nextLine();
+
+        Pattern pattern = Pattern.compile("([а-яА-Я]+)(?: ([а-яА-Я]+))? = (\\d+)?;?");
         Matcher matcher = pattern.matcher(inputText);
+
+
         while (matcher.find()) {
             String firstName = matcher.group(1);
             String lastName = matcher.group(2);
+            String name = firstName + " " + lastName;
             int money = Integer.parseInt(matcher.group(3));
-            if (lastName == null) {
-                System.out.println("First Name: " + firstName + ", Amount: " + money);
-            } else {
-                System.out.println("First Name: " + firstName + " " +  lastName + ", Amount: " + money);
-            }
+            Person person = new Person(name, money);
 
+            if (lastName == null) {
+                person.setName(firstName);
+
+//                System.out.println("First Name: " + firstName + ", Amount: " + money); -проверка
+            } else {
+                person.setName(firstName + " " + lastName);
+
+            }
+            person.setMoney(money);
+
+            // System.out.println("First Name: " + firstName + " " +  lastName + ", Amount: " + money); проверка
             Scanner scannerProduct = new Scanner(System.in);
+            System.out.println("Введите наименование товара и стоимость товара: ");
             String inputTextProduct = scannerProduct.nextLine();
-            Pattern patternProduct = Pattern.compile("([а-яА-Я]+) = (\\d+);");
+            Pattern patternProduct = Pattern.compile("([а-яА-Я]+)(?: ([а-яА-Я]+))? = (\\d+)?;?");
             Matcher matcherProduct = patternProduct.matcher(inputTextProduct);
             while (matcherProduct.find()) {
-                String nameProduct = matcherProduct.group(1);
-                int amount = Integer.parseInt(matcherProduct.group(2));
+                String firstWord = matcherProduct.group(1);
+                String lastWord = matcherProduct.group(2);
+           String productName = firstWord + " " + lastWord;
+                int productPrice = Integer.parseInt(matcherProduct.group(3));
 
-                System.out.println("Name: " + nameProduct + ", Amount: " + amount);
+                Product product = new Product(productName, productPrice);
+                if (lastWord == null) {
 
+                    product.setProductName(firstWord );
+              } else {
+
+                    product.setProductName(firstWord + " " + lastWord);
                 }
 
+                product.setProductPrice(productPrice);
+                if (person.getMoney() < product.getProductPrice()) {
+                    System.out.println(person.getName() + " -  Ничего не куплено  " + product.getProductName());
+
+                } else if (productName.equals("Торт")) {
+                    System.out.println(person.getName() + " не может позволить себе " + product.getProductName());
+                } else {
+
+                    System.out.println(person.getName() + " купил " + product.getProductName());
+                }
+
+//                System.out.println("Name: " + nameProduct + ", Amount: " + amount); проверка
+
+           }
 
         }
     }
@@ -40,16 +75,5 @@ public class App {
 
 
 
-
-
-//        Scanner scan = new Scanner(System.in);
-//        boolean check = true;
-//        while (check) {
-//            String input = scan.nextLine();
-//
-//            if (input.equals("END")) {
-//                check = false;
-//            }
-//        }
 
 
