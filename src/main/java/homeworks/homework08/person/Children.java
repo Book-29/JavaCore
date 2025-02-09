95a10137d9599fc0364aa9d90fc5cacc88daba6d
Лариса <book-29@mail.ru>
2025-02-09 17:00:28 +0300

    домашнеe задания 8

---
+++ src/main/java/homeworks/homework08/person/Children.java
@@ -0,0 +1,24 @@
+package homeworks.homework08.person;
+
+
+
+import java.util.Objects;
+
+public class Children extends Person {
+
+
+public Children(String name, double money, int age) {
+   super(name, money, age);
+
+    }
+
+
+    @Override
+    public String toString() {
+        return "Children{" +
+                "name='" + super.getName() +
+                ", money=" + super.getMoney() +
+                ", age=" + super.getAge() +
+                '}';
+    }
+}
